9a4cb01ae1f0ded5ccdf9bc3c119cad0f53afa81
GGOOPARK <104619482+GGOOPARK@users.noreply.github.com>
2022-09-07 18:22:59 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc1 = new Scanner(System.in);
+        double A,B;
+        A = sc1.nextFloat();
+        B = sc1.nextFloat();
+        System.out.println(A/B);
+    }
+}
