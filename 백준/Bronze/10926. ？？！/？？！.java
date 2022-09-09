0ba6cb4ed76283b9fec14e9d23f6619e8ef10ca4
GGOOPARK <104619482+GGOOPARK@users.noreply.github.com>
2022-09-09 14:58:47 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc1 = new Scanner(System.in);
+        String A;
+        A = sc1.next();
+        System.out.println(A+"??!");
+        }
+}
