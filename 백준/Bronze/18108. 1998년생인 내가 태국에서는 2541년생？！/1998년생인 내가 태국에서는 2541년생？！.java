e8630b2343ef85b24f523ded4d2d3d111418c35f
GGOOPARK <104619482+GGOOPARK@users.noreply.github.com>
2022-09-10 20:04:03 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc1 = new Scanner(System.in);
+        int A;
+        A = sc1.nextInt();
+        System.out.println(A-543);
+        }
+}
