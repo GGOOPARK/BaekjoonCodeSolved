a3eb99680c8b774503573b31a71294671e15357b
GGOOPARK <104619482+GGOOPARK@users.noreply.github.com>
2022-10-04 23:35:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        char B = A.charAt(0);
+        int C = B;
+        System.out.println(C);
+    }
+}
